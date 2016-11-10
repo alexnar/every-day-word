f143ddea3db2cd44591e2da15d85e038741bb569
alexnar <sasha_nar@mail.ru>
2016-11-10 22:16:25 +0300

    admin action aprove new words

--- dict/admin.py
+++ dict/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from dict.models import Dictionary, TodayWord, UseExample, NewWord
 
-# Register your models here.
 
+def approve_words(modeladmin, request, word_set):
+    for word in word_set:
+        dictionary = Dictionary(word=word.word, definition=word.definition, author=word.author)
+        dictionary.save()
+        word.delete()
+
+
+class NewWordAdmin(admin.ModelAdmin):
+    list_display = ['word', 'definition', 'author']
+    actions = [approve_words]
+
+
+admin.site.register(NewWord, NewWordAdmin)
 admin.site.register(Dictionary)
 admin.site.register(TodayWord)
 admin.site.register(UseExample)
-admin.site.register(NewWord)
